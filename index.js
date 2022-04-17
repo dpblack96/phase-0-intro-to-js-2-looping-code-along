1598cb5c2c08659b6fc9e659bace7f08c6b5236f
Dominic Black <dpblack96@gmail.com>
2022-04-16 22:08:52 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(names, typeOfGift) {
+    let messages = []
+    
+    for (let i = 0; i < names.length; i++) {
+        let message = `Thank you, ${names[i]}, for the wonderful ${typeOfGift} gift!`
+        console.log (message)
+        messages.push(message)
+    }
+    return messages
+}
+
+function countDown() {
+    let j = 10;
+    while (j >= 0) {
+        console.log (j);
+        j--
+    }
+    return j
+}
